51bbc9de80c87346843d1a3d9493d48824daabf9
active-collaborator-0000 <dianamayer0@gmail.com>
2019-08-30 15:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,17 @@ function sayHiToGrandma(string){
 */
 
 function sayHiToGrandma(hear) {
-  if (hear.toLowerCase() === string) {
-    return 'I can\'t hear you!'
-  }}
-
-  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+      return "I can't hear you!"
+    }
+  
     if (string.toUpperCase() === string) {
       return "YES INDEED!"
-    }}
+    }
+  
+    if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+  
+    return "Are you eating enough?"
+  }

3a7d4a9959f75e85f9ca8467f1a2d070fdfdf4c0
active-collaborator-0000 <dianamayer0@gmail.com>
2019-08-30 15:47:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(whisper) {
+  return whisper.toLowerCase()
+}
+
+function logShout(hello) {
+console.log(hello.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+/*
+function sayHiToGrandma(string){
+  string.toLowerCase() === lowercase
+  return "i can't hear you!"
+}
+
+*/
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }}
